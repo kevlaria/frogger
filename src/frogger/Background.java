db0c2bf5d1b113915084b2e2da7623fbb52505d8
kevlaria <kevlaria@gmail.com>
2013-11-24 11:03:39 -0500

    Rejigged files into the main java folder under frogger package + updated bg -> Background.java

--- src/frogger/Background.java
+++ src/frogger/Background.java
@@ -1,10 +1,46 @@
 package frogger;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Background {
-	
-	String background;
-	
-	public Background(){
-		
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Button;
+
+
+public class Background extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Background frame = new Background();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Background() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		Button button = new Button("New button");
+		contentPane.add(button, BorderLayout.WEST);
+	}
+
 }
