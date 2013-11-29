03f304d57dd7ae4e2d4ac1cf317dc84501c39862
kevlaria <kevlaria@gmail.com>
2013-11-29 11:17:28 -0500

    Updated with frog controller and object

--- src/frogger/Frog.java
+++ src/frogger/Frog.java
@@ -1,5 +1,7 @@
 package frogger;
 
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,14 +9,33 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 
+
+
 public class Frog extends Sprite {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	private Image image;
+	protected View view;
+	boolean Alive;
+	Orientation direction;
+	public enum Orientation { UP, RIGHT, DOWN, LEFT };
+	
+	int maxY = 1000;
+	int maxX = 1000;
+	
+  //  private static final int leftMostXCoordinate = -50;
+   // private static final int rightMostXCoordinate = 700;
 
+	/**
+	 * Constructor for frog
+	 */
+	public Frog()
+	{
+		this.image = loadImage("src/Images_for_Frogger/frog-up.png");
+		direction = Orientation.UP;
+		Alive = true;
+		this.x =200;
+		this.y =200;
 	}
 	
 
@@ -23,23 +44,251 @@ public class Frog extends Sprite {
 	@Override
 	void update() {
 		// TODO Auto-generated method stub
+		//this.image = loadImage("src/Images_for_Frogger/frog-right.png");
 		
+		
+		//Update the current position
+		dx=0;
+		dy=0;
+		moveForward();
+		x = x + dx;
+		y = y + dy;
+		//this.x += 100;
+		setChanged();
+    	notifyObservers(); 
 	}
 
-	@Override
-	void draw() {
-		// TODO Auto-generated method stub
-		
-		BufferedImage img = null;
-		try {
-		    img = ImageIO.read(new File("frog.jpg"));
-		}
-		catch (IOException e) {
-	//	throw new IOExceptionError;
-		
-		}
-		
+	public void setDirection(Orientation position){
+		this.direction = position;
+	}
+	
+	
+	public Orientation getDirection()
+	{
+		return this.direction;
+	}
+	
 
+	
+	
+	/**
+	 * Method Move Forward:
+	 * This gets the current orientation of the object.
+	 * When the user pushes the up button, this code is activated.
+	 * When this method is called, it will move the position of the frog 
+	 * + or - 1 depending on the current orientaiton of the frog.
+	 */
+	public void moveForward()
+	{
+		if (direction.toString() == "LEFT")
+		{
+			if ((x-1) >= -1000)
+			{
+			// OR UPDATE DX
+				dx = -10;
+			}
+			
+		}
+		else if (direction.toString() == "UP")
+		{
+			//if ((y+1) >= maxY)
+			//{
+				dy = -10;
+	//		}
+			
+		}
+		else if (direction.toString() == "DOWN")
+		{
+			//if ((y+1) <= -1000)
+			//{
+				dy= +10;
+			//}
+		}
+		else if (direction.toString() == "RIGHT")
+		{
+			if ((x+1) <= maxX)
+			{
+			 dx= +10;
+			}
+		}
+		
+	}
+	
+	   
+	public void rotateClockwise()
+	{
+		if (direction.toString() == "LEFT")
+		{
+			this.direction = Orientation.UP;
+			reloadImage(Orientation.UP);
+		}
+		else if (direction.toString() == "UP")
+		{
+			this.direction = Orientation.RIGHT;
+			reloadImage(Orientation.RIGHT);
+		}
+		else if (direction.toString() == "RIGHT")
+		{
+			this.direction = Orientation.DOWN;
+			reloadImage(Orientation.DOWN);
+
+		}
+		else if (direction.toString() == "DOWN")
+		{
+			this.direction = Orientation.LEFT;
+			reloadImage(Orientation.LEFT);
+		}
+		
+	}
+	
+	
+	
+	public void rotateCounterClockwise()
+	{
+		if (direction.toString() == "LEFT")
+		{
+			this.direction = Orientation.DOWN;
+			reloadImage(Orientation.DOWN);
+		}
+		else if (direction.toString() == "UP")
+		{
+			this.direction = Orientation.LEFT;
+			reloadImage(Orientation.LEFT);
+		}
+		else if (direction.toString() == "RIGHT")
+		{
+			this.direction = Orientation.UP;
+			reloadImage(Orientation.UP);
+
+		}
+		else if (direction.toString() == "DOWN")
+		{
+			this.direction = Orientation.RIGHT;
+			reloadImage(Orientation.RIGHT);
+		}
+		
+	}
+	
+	
+	public void rotate180()
+	{
+		if (direction.toString() == "LEFT")
+		{
+			this.direction = Orientation.RIGHT;
+			reloadImage(Orientation.RIGHT);
+		}
+		else if (direction.toString() == "UP")
+		{
+			this.direction = Orientation.DOWN;
+			reloadImage(Orientation.DOWN);
+		}
+		else if (direction.toString() == "RIGHT")
+		{
+			this.direction = Orientation.LEFT;
+			reloadImage(Orientation.LEFT);
+
+		}
+		else if (direction.toString() == "DOWN")
+		{
+			this.direction = Orientation.UP;
+			reloadImage(Orientation.UP);
+		}
+		
+	}
+	/**
+	 * Loads the correct image based on the direction
+	 * 
+	 */
+	public void reloadImage(Orientation direction) {
+				
+		// TODO Draw the position based on the position passed in
+		if (direction == Orientation.LEFT)
+		{
+			
+			this.image = loadImage("src/Images_for_Frogger/frog-left.png");
+			setChanged();
+	    	notifyObservers(); 
+	    
+
+		}
+		else if (direction == Orientation.UP)
+		{
+			this.image = loadImage("src/Images_for_Frogger/frog-up.png");
+			setChanged();
+	    	notifyObservers(); 	
+		}
+		else if (direction == Orientation.DOWN)
+		{
+			this.image = loadImage("src/Images_for_Frogger/frog-down.png");
+			setChanged();
+	    	notifyObservers(); 
+		}
+		else if (direction == Orientation.RIGHT)
+		{
+			this.image = loadImage("src/Images_for_Frogger/frog-right.png");
+			setChanged();
+	    	notifyObservers(); 
+		}	
+	
+	}
+		
+///////////////////////////
+
+    /**
+     * Method to load image
+     * @param fileName
+     * @return
+     */
+    public Image loadImage(String fileName){
+            BufferedImage img = null;
+            try {
+                img = ImageIO.read(new File(fileName));
+            }
+            catch (IOException e) {
+        System.out.println("Can't load image.");
+            }
+            return img;
+            
+    }
+    
+
+
+	@Override
+	String getSpriteType() {
+		// TODO Auto-generated method stub
+		return "Frog";
+	}
+
+
+
+	@Override
+	Image getImage() {
+		// TODO Auto-generated method stub
+		return this.image;
+	}
+
+
+	@Override
+	int getX() {
+		// TODO Auto-generated method stub
+		return this.x;
+	}
+
+
+	@Override
+	int getY() {
+		// TODO Auto-generated method stub
+		return this.y;
+	}
+
+
+
+
+
+	@Override
+	void draw(Graphics g) {
+		
+		g.drawImage(this.image, x, y, this.view);	
 		
 	}
 
--- src/frogger/Frogger.java
+++ src/frogger/Frogger.java
@@ -1,12 +1,12 @@
 package frogger;
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,20 +14,24 @@ import java.util.TimerTask;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+
 
 
 public class Frogger extends JFrame implements Runnable{
 
-	private Cast cast;
-	private View view;
-	private JFrame frame;
+	Cast cast;
+	View view;
+	JFrame frame;
 	private Background background;
 	private Timer timer;
 	private JButton goButton;
 	private JButton pauseButton;
 	private JPanel buttonPanel;
 	private int carCount;
+	public enum Orientation { UP, RIGHT, DOWN, LEFT };
+
+	protected Frog frog;
+
 	
 	/**
 	 * Launch the application.
@@ -45,19 +49,25 @@ public class Frogger extends JFrame implements Runnable{
 	}
 	
 	public void run(){	
-		
+
 		/**
 		 * Controller assigns Background and View to each other, as well as the controller
 		 */
 		this.view.controller = this;		
 		background.view = this.view;
 		
+		
 		this.cast.addSprites(this.createNewCar());
 		this.cast.addSprites(this.createNewCar());
 		this.cast.addSprites(this.createNewCar());
 		this.cast.addSprites(this.createNewCar());
 		this.cast.addSprites(this.createNewCar());
 		this.cast.addSprites(this.createNewCar());
+	
+		
+		//Add Frog
+		this.frog = this.createNewFrog();
+		this.cast.addSprites(frog);
 		
 		this.frame = this.drawJFrame();
  		this.frame.add(view, BorderLayout.CENTER);
@@ -65,8 +75,63 @@ public class Frogger extends JFrame implements Runnable{
  		this.buttonPanel = this.createButtonPanel();
  		this.frame.add(buttonPanel, BorderLayout.SOUTH);
 		this.frame.setVisible(true);
+		this.frame.requestFocus();
+		pack();
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    
+        this.frame.addKeyListener(new KeyListener() {
+          
+			//Cast cast = getCast();
+			//ArrayList<Sprite> spriteList = cast.getSprites();
+		
+			@Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_UP) {
+                    System.out.println("Up arrow pressed.");
+  
+                    //frog.setDirection(Orientation.LEFT);
+                    //Should call method to move forward
+                    frog.update();
+                    frame.requestFocus();
+                    frame.repaint();
+       
+                }
+                if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    System.out.println("Down arrow pressed.");
+                    frog.rotate180();
+                    frame.requestFocus();
+                    frame.repaint();
+        
+                    
+                }
+                if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    System.out.println("Left arrow pressed.");
+                    frog.rotateCounterClockwise();
+                    frame.requestFocus();
+                    frame.repaint();
+                }
+                if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    System.out.println("Right arrow pressed.");
+                    frog.rotateClockwise();
+                    frame.requestFocus();
+                    frame.repaint();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("Released key " + e.getKeyCode());
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                System.out.println("Typed key " + e.getKeyChar());
+            }
+        });
 	}
-	
+		   
+
+
 	/**
 	 * Method to create the frame for the game
 	 * @return The frame for the game
@@ -78,6 +143,8 @@ public class Frogger extends JFrame implements Runnable{
 	 	frame.setResizable(false);
 	 	frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
 		return frame;
 	}
 	
@@ -92,7 +159,9 @@ public class Frogger extends JFrame implements Runnable{
 		// add buttons
 		
 		this.goButton = new JButton("Go!");
+		goButton.setFocusable(false);
 		this.pauseButton = new JButton("Pause");
+		pauseButton.setFocusable(false);
 		panel.add(goButton);
 		panel.add(pauseButton);
 		this.attachListenersToButtons();
@@ -113,6 +182,17 @@ public class Frogger extends JFrame implements Runnable{
 	}
 	
 
+	/**
+	 * Method to create a new Car object
+	 * @return a Car object
+	 */
+	public Frog createNewFrog(){
+		Frog frog = new Frog();
+		frog.view = this.view;
+		frog.addObserver(view);
+		return frog;
+	}
+	
 	/***********
 	 * ADDING BUTTON ACTION COMPONENTS
 	 * *********
@@ -152,7 +232,8 @@ public class Frogger extends JFrame implements Runnable{
 			Cast cast = getCast();
 			ArrayList<Sprite> spriteList = cast.getSprites();
 	    	int castSize = spriteList.size();
-	    	for (int i = 0; i < castSize; i++){
+	    	//Every Sprite except the last sprite, Frog 
+	    	for (int i = 0; i < castSize-1; i++){
 	    		Sprite sprite = spriteList.get(i);
 	    		sprite.update();
 	    	}
